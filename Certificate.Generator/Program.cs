9302626266d69c5d3c13d78ebe488ef2ab3d75bc
Benjamin Cartier <benjamin.cartier@nventive.com>
2019-11-08 14:16:53 -0500

    Add project files.

---
+++ Certificate.Generator/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using CertificateGenerator.Extensions;
+
+namespace CertificateGenerator
+{
+	public static class Program
+	{
+		private static Lazy<string> _temporaryDirectoryPath = new Lazy<string>(CreateTemporaryDirectory);
+
+		/// <summary>
+		/// Helper tool to create Apple certificates.
+		/// This program creates a certificate signing request to be used in the Apple Developer console to obtain a certificate. 
+		/// A P12 is then generated using this certificate.
+		/// </summary>
+		/// <param name="emailAddress">A relevant email address to use in the request (ex: the mail of the owner of the account)</param>
+		/// <param name="commonName">The name to use in the request; usually the name of the Apple Developer organization</param>
+		/// <param name="countryCode">The country code of the account</param>
+		/// <param name="requestFilePath">Optional: the path where to place the .csr file</param>
+		/// <param name="p12FilePath">Optional: the path where to place the .p12 file</param>
+		/// <returns></returns>
+		public static int Main(
+			string emailAddress,
+			string commonName,
+			string countryCode,
+			string requestFilePath = null,
+			string p12FilePath = null
+		)
+		{
+			//Use temporary files in nothing is specified
+			requestFilePath = requestFilePath ?? GetTemporaryFilePath("csr");
+			p12FilePath = p12FilePath ?? GetTemporaryFilePath("p12");
+
+			// Generate a new key
+			var key = RSA.Create(2048);
+
+			//Create the CSR
+			var request = CreateCertificateRequest(key, emailAddress, commonName, countryCode);
+			var requestContent = request.GetPemEncodedSigningRequest();
+
+			//Save the CSR
+			File.WriteAllText(requestFilePath, requestContent);
+
+			Console.WriteLine($"Created {requestFilePath} to use to create a certificate.");
+			
+			var certificateFilePath = ReadLine.Read("Location of the .cer file:");
+
+			//Load the certificate and include the private key
+			var certificate = new X509Certificate2(X509Certificate.CreateFromCertFile(certificateFilePath)).CopyWithPrivateKey(key);
+
+			var password = ReadLine.ReadPassword("Enter the password to use for the p12:");
+
+			//Export to a p12
+			var content = certificate.Export(X509ContentType.Pkcs12, password);
+
+			//Save the p12
+			File.WriteAllBytes(p12FilePath, content);
+
+			Console.WriteLine($"Successfully exported certificate under {p12FilePath}");
+
+			return 0;
+		}
+		
+		private static CertificateRequest CreateCertificateRequest(RSA key, string emailAddress, string commonName, string countryCode)
+		{
+			var request = new CertificateRequest($"CN={commonName}, C={countryCode}", key, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+
+			//The email address needs to be added to the request as an alternative name
+			var alternativeNameBuilder = new SubjectAlternativeNameBuilder();
+
+			alternativeNameBuilder.AddEmailAddress(emailAddress);
+
+			request.CertificateExtensions.Add(alternativeNameBuilder.Build());
+
+			return request;
+		}
+
+		private static string CreateTemporaryDirectory() => Directory.CreateDirectory(Path.Combine(Path.GetTempPath(), $"CertificateGenerator.{Guid.NewGuid()}")).FullName;
+
+		private static string GetTemporaryFilePath(string extension) => Path.Combine(_temporaryDirectoryPath.Value, $"{Guid.NewGuid()}.{extension}");
+	}
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+# Certificate Generator
+
+Create Apple certificate from anywhere, not just macOS.
+
+This tool allows the creation of certificate signing requests compatible with Apple Developer console. Once the certificate has been created, the tool also creates a `.p12` file for safekeeping.
+
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](LICENSE)
+
+## Getting Started
+
+Simply download the code, restore its NuGet packages and get dem certificates!
+
+## Features
+
+- Creation of `.csr` (or `.certSigningRequest`) files compatible with the Apple Developer console (Push, Distribution, Development, etc.)
+- Creation of the `.p12` file corresponding to the `.cer` delivered by Apple.
+
+## Changelog
+
+Please consult the [CHANGELOG](CHANGELOG.md) for more information about version
+history.
+
+## License
+
+This project is licensed under the Apache 2.0 license - see the
+[LICENSE](LICENSE) file for details.
+
+## Contributing
+
+Please read [CONTRIBUTING.md](CONTRIBUTING.md) for details on the process for
+contributing to this project.
+
+Be mindful of our [Code of Conduct](CODE_OF_CONDUCT.md).
+
+## Acknowledgments
