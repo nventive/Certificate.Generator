0e0b3fa77c89d3d4394ca5760ddd96bd9a55a0e8
Benjamin Cartier <benjamin.cartier@nventive.com>
2019-11-12 17:23:43 -0500

    Added missing code file

---
+++ Certificate.Generator/Extensions/CertificateRequestExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace CertificateGenerator.Extensions
+{
+    internal static class CertificateRequestExtensions
+    {
+		private const string BeginCertificateRequestLine = "-----BEGIN CERTIFICATE REQUEST-----";
+		private const string EndCertificateRequestLine = "-----END CERTIFICATE REQUEST-----";
+
+		private const int ContentLineLength = 64;
+
+		/// <summary>
+		/// Gets the PEM-encoded content for the given <see cref="CertificateRequest"/>.
+		/// Adapted version of Microsoft code: <see href="https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography.x509certificates.certificaterequest.createsigningrequest?view=netframework-4.8#System_Security_Cryptography_X509Certificates_CertificateRequest_CreateSigningRequest" />
+		/// </summary>
+		/// <param name="request">The certificate request to get the signing request from</param>
+		/// <returns>The PEM-encoded signing request</returns>
+		public static string GetPemEncodedSigningRequest(this CertificateRequest request)
+		{
+			var builder = new StringBuilder();
+
+			builder.AppendLine(BeginCertificateRequestLine);
+
+			var content = Convert.ToBase64String(request.CreateSigningRequest());
+
+			int offset = 0;
+
+			while (offset < content.Length)
+			{
+				int lineEnd = Math.Min(offset + ContentLineLength, content.Length);
+
+				builder.AppendLine(content.Substring(offset, lineEnd - offset));
+
+				offset = lineEnd;
+			}
+
+			builder.AppendLine(EndCertificateRequestLine);
+
+			return builder.ToString();
+		}
+	}
+}
